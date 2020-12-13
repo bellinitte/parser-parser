e6f43386cfb54e85d72cec4a84c30bfedc0d3993
Karol Belina <karolbelina@gmail.com>
2020-12-14 00:36:53 +0100

    Remove the target folder

--- cli/target/debug/deps/cli-2497bf2af2bc26c2.d
+++
@@ -1,5 +0,0 @@
-/mnt/c/Users/karol/Projects/parser-parser/cli/target/debug/deps/cli-2497bf2af2bc26c2.rmeta: src/main.rs
-
-/mnt/c/Users/karol/Projects/parser-parser/cli/target/debug/deps/cli-2497bf2af2bc26c2.d: src/main.rs
-
-src/main.rs:
